a243a428e85ade2ac5e08a2302af8836c0e9b148
kincajr-ux <kincajr@hotmail.com>
2026-07-05 02:22:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{
+threshold:0.2
+});
+
+cards.forEach(card=>observer.observe(card));
+
+const botoes = document.querySelectorAll(".botao");
+
+botoes.forEach(botao=>{
+
+botao.addEventListener("mouseenter",()=>{
+
+botao.style.transform="scale(1.05)";
+
+});
+
+botao.addEventListener("mouseleave",()=>{
+
+botao.style.transform="scale(1)";
+
+});
+
+});
+
+});
